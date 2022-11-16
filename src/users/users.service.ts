4ac3bcba28ca61c70acd4d71f93c33a0cd05b410
omobosteven <omobosteven@gmail.com>
2022-11-16 23:47:10 +0100

    generate user modules files

---
+++ src/users/users.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+}
